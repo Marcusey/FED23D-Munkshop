5705da92252407be9e52bc8ce1fe7fe69ac89816
Marcusey <146931671+Marcusey@users.noreply.github.com>
2024-06-12 08:32:53 +0200

    :construction: test 6

--- main.js
+++ main.js
@@ -58,9 +58,9 @@ function checkFormValidity() {
 
 //--Hårdkodat sprayburkar/produkter--//
 const sprayCans = [
-  { id: 1, name: 'CanCan in Sunset', price: 100, image: { src: './assets/pictures/orange.JPG', alt: 'Spray Can 1 Image'}, amount: 0, stars: 4, category: 'light' },
-  { id: 2, name: 'Yellow Mr. Sunshine', price: 120, image: { src: 'assets/pictures/yellow.JPG', alt: 'Spray Can 2 Image'}, amount: 0, stars: 3.5, category: 'light' },
-  { id: 3, name: 'Red Rush ', price: 140, image: { src: 'assets/pictures/red.jpg', alt: 'Spray Can 3 Image' }, amount: 0, stars: 5, category: 'light' },
+  { id: 1, name: 'CanCan in Sunset', price: 100, image: {src:'/FED23D-Munkshop/assets/pictures/orange.jpg', alt: 'Spray Can 1 Image'}, amount: 0, stars: 4, category: 'light' },
+  { id: 2, name: 'Yellow Mr. Sunshine', price: 120, image: {src: 'assets/pictures/yellow.JPG', alt: 'Spray Can 2 Image'}, amount: 0, stars: 3.5, category: 'light' },
+  { id: 3, name: 'Red Rush ', price: 140, image: {src: 'assets/pictures/red.jpg', alt: 'Spray Can 3 Image' }, amount: 0, stars: 5, category: 'light' },
   { id: 4, name: 'Pink that Party', price: 110, image: { src: 'assets/pictures/pink.jpg', alt: 'Spray Can 4 Image' }, amount: 0, stars: 4.5, category: 'pastel' },
   { id: 5, name: 'Bloom Fusia, bloom', price: 110, image: { src: 'assets/pictures/fusia.jpg', alt: 'Spray Can 5 Image' }, amount: 0, stars: 3, category: 'light' },
   { id: 6, name: 'Cast a spell', price: 100, image: { src: 'assets/pictures/chrome.jpg', alt: 'Spray Can 6 Image' }, amount: 0, stars: 4, category: 'light' },
